4b8b240cca7cd40eed2c96181718720c0299255c
Foxtrot <FoxtrotBravo404@gmail.com>
2014-08-11 14:34:19 +0100

    Added Nedry Roll by Barry99705

---
+++ assets/rolls/NedryRoll/nedry.php
@@ -0,0 +1,18 @@
+<HTML>
+<head>
+	<!-- nedry roll for RandomRoll     Created by barry99705 php borrowed from foxtrot! -->
+
+<title> God damn it! I hate this hacker crap!! </title> 
+</head> 
+<body bgcolor="white">
+<center>
+<img src="assets/rolls/nedry/nedry.gif" align="middle" width="500" height="500" /> 
+</center>
+<EMBED SRC=assets/rolls/nedry/ahahahh.mp3 AUTOSTART=true WIDTH=1 HEIGHT=1 LOOP=true>
+
+   <audio autoplay="true" loop="loop" src="assets/rolls/nedry/ahahahh.mp3">
+     <source src="assets/rolls/nedry/ahahahh.mp3" type="audio/mp3" />
+   </audio>
+
+</body>
+</HTML>
